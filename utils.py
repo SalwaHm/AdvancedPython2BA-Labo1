6970c4edc4c2ff4c3c84b453240a283a0b21bfab
SalwaHm <22110@ecam.be>
2024-02-20 11:54:37 +0100

    nouvelle_version

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from scipy.integrate import quad
+import numpy as np
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -5,7 +7,15 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	res = 1
+	if n == 0:
+		res = 1
+	if n > 0:
+		for i in range(1,n+1):
+			res*=i 
+	return res
+
+	#pass
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -14,7 +24,26 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	delta = 0
+	delta = (b**2)-(4*a*c)
+	res = ()
+	x_1 = 0
+	if delta < 0:
+
+		res = ()
+	if delta == 0:
+
+		x_1 = -b/(2*a)
+		res = (x_1)
+
+	if delta > 0: 
+		x_1 = (-b + (delta)**(1/2))/(2*a)
+		x_2 = (-b - (delta)**(1/2))/(2*a)
+		res = (x_1, x_2)
+	
+	return res
+
+	#pass
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -29,7 +58,10 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	resultat = quad(lambda x: eval(function), lower, upper)[0]
+	return resultat
+	
+	#pass
 
 if __name__ == '__main__':
 	print(fact(5))
